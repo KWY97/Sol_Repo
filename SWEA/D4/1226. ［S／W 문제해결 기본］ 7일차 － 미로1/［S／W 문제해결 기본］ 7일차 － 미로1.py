324b5193572f39449ccad248373dd38b6f099de7
KWY97 <kwy1547@gmail.com>
2024-09-08 21:01:04 +0900

    [D4] Title: [S/W 문제해결 기본] 7일차 - 미로1, Time: 146 ms, Memory: 49,196 KB -BaekjoonHub

--- SWEA/D4/1226. ［S／W 문제해결 기본］ 7일차 － 미로1/［S／W 문제해결 기본］ 7일차 － 미로1.py
+++ SWEA/D4/1226. ［S／W 문제해결 기본］ 7일차 － 미로1/［S／W 문제해결 기본］ 7일차 － 미로1.py
@@ -1,43 +1,41 @@
-def find_start():
-    for i in range(size):
-        for j in range(size):
-            if arr[i][j] == '2':
+from collections import deque
+
+def search_start():
+# 시작점 찾기
+    for i in range(N):
+        for j in range(N):
+            if arr[i][j] == 2:
                 return i, j
 
-def search_road(st_x, st_y):
-    visited = [[0] * (size) for _ in range(size)]
-    stack = []
+def my_bfs(i, j):
+    queue.append([i, j])
+    visited[i][j] = 1
 
-    x, y = st_x, st_y
-    visited[x][y] = 1
+    while queue:
+        x, y = queue.popleft()
 
-    while True:
+        if arr[x][y] == 3:
+            return 1
+
         for dx, dy in dxy:
             nx = x + dx
             ny = y + dy
 
-            if nx < 0 or ny < 0 or nx >= size or ny >= size or visited[nx][ny] == 1 or arr[nx][ny] == '1':
+            if nx < 0 or ny < 0 or nx >= N or ny >= N or visited[nx][ny] == 1 or arr[nx][ny] == 1:
                 continue
-            if arr[nx][ny] == '3':
-                return 1
 
-            stack.append([x, y])
-            x, y = nx, ny
+            queue.append([nx, ny])
             visited[nx][ny] = 1
-            break
-        else:
-            if stack:
-                temp = stack.pop()
-                x, y = temp[0], temp[1]
-            else:
-                return 0
+    return 0
 
-size = 16
-dxy = [[0, 1], [1, 0], [0, -1], [-1, 0]]
 
-for _ in range(1, 11):
+T = 10
+N = 16
+dxy = [[1, 0], [0, 1], [-1, 0], [0, -1]]
+for _ in range(T):
     tc = int(input())
-    arr = [list(input()) for _ in range(size)]
-    start_x, start_y = find_start()
-    ans = search_road(start_x, start_y)
-    print(f'#{tc} {ans}')
+    arr = [list(map(int, input())) for _ in range(N)]
+    si, sj = search_start()
+    queue = deque()
+    visited = [[0] * (N) for _ in range(N)]
+    print(f'#{tc} {my_bfs(si, sj)}')
