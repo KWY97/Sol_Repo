2f0570e55ec46e0bbe8695b7d8840a9cac27f3f7
KWY97 <kwy1547@gmail.com>
2024-09-11 16:07:26 +0900

    [Silver IV] Title: 설탕 배달, Time: 88 ms, Memory: 109444 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.py
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.py
@@ -1,11 +1,30 @@
+import sys
+input = sys.stdin.readline
+
+def my_dfs(n, sum):
+    global ans
+
+    if sum > N:
+        return
+
+    if sum == N:
+        ans = n
+        return
+
+    if n >= ans:
+        return
+
+    if ans == 1667:
+        if sum + 5 <= N:
+            my_dfs(n+1, sum + 5)
+        if sum + 3 <= N:
+            my_dfs(n+1, sum + 3)
+
 N = int(input())
-ans = 0
-while N >= 0:
-    if N % 5 == 0:
-        ans += N // 5
-        print(ans)
-        break
-    N -= 3
-    ans += 1
+ans = 1667
+my_dfs(0, 0)
+
+if ans != 1667:
+    print(ans)
 else:
     print(-1)
