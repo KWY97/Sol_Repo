34c9caff3e658a244a38fdea0499cf096d359170
KWY97 <kwy1547@gmail.com>
2024-10-08 11:00:27 +0900

    [Silver V] Title: 다리 놓기, Time: 120 ms, Memory: 110404 KB -BaekjoonHub

--- 백준/Silver/1010. 다리 놓기/다리 놓기.py
+++ 백준/Silver/1010. 다리 놓기/다리 놓기.py
@@ -1,10 +1,3 @@
-def f():
-    temp = []
-    for v in range(M, 0, -1):
-        temp.append(v)
-        if len(temp) == N:
-            return temp
-
 def fac(n):
     temp_num = 1
     for i in range(1, n+1):
@@ -15,8 +8,5 @@ T = int(input())
 
 for _ in range(T):
     N, M = map(int, input().split())
-    num = 1
-    my_li = f()
-    for v in my_li:
-        num *= v
-    print(num//fac(N))
+
+    print(fac(M) // (fac(N) * fac(M-N)))
