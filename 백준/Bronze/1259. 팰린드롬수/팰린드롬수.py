3f34a89fc89f3b0f00fa3da0e1a5806484bc00ca
KWY97 <kwy1547@gmail.com>
2024-11-01 00:01:14 +0900

    [Bronze I] Title: 팰린드롬수, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/1259. 팰린드롬수/팰린드롬수.py
+++ 백준/Bronze/1259. 팰린드롬수/팰린드롬수.py
@@ -1,32 +1,10 @@
-def check(number):
-    length = len(number)
-
-    if length == 1:
-        return 'yes'
-    elif length == 2:
-        if number[0] == number[1]:
-            return 'yes'
-        else:
-            return 'no'
-    elif length == 3:
-        if number[0] == number[2]:
-            return 'yes'
-        else:
-            return 'no'
-    else:
-        new_length = (length // 2)
-        for i in range(new_length):
-            if number[i] == number[(length-1) - i]:
-                continue
-            else:
-                return 'no'
-        return 'yes'
-
-
 while True:
-    number = input()
-
-    if number == '0':
+    n = input()
+    if n=='0':
         break
-
-    print(check(number))
+    for i in range(len(n)//2):
+        if n[i]!=n[-1-i]:
+            print("no")
+            break
+    else:
+        print('yes')
