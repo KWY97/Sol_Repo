5d4d96eae9ac709c74ebba39d5bb16a73536c7dd
KWY97 <kwy1547@gmail.com>
2025-05-01 15:37:04 +0900

    [Silver IV] Title: 나는야 포켓몬 마스터 이다솜, Time: 196 ms, Memory: 56744 KB -BaekjoonHub

--- 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.py
+++ 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.py
@@ -2,17 +2,21 @@ import sys
 input = sys.stdin.readline
 
 n, m = map(int, input().split())
-
-pocketmons = [input().strip() for _ in range(n)]
-problem = [input().strip() for _ in range(m)]
 ans = []
 
-for x in problem:
-    if x.isalpha() == False:
-        target_idx = (int(x)-1)
-        ans.append(pocketmons[target_idx])
+pocketmon_name_to_num = {}
+pocketmon_num_to_name = {}
+
+for i in range(1, n + 1):
+    pocketmon_name = input().strip()
+    pocketmon_name_to_num[pocketmon_name] = i
+    pocketmon_num_to_name[i] = pocketmon_name
+
+for _ in range(m):
+    problem = input().strip()
+    if problem.isalpha():
+        ans.append(pocketmon_name_to_num[problem])
     else:
-        target_name = x
-        ans.append((pocketmons.index(target_name))+1)
+        ans.append(pocketmon_num_to_name[int(problem)])
 
 print("\n".join(map(str, ans)))
