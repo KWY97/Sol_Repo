599c26c007d40b0bea29a14ec184ca07ee14e27f
KWY97 <kwy1547@gmail.com>
2025-06-13 16:59:02 +0900

    [Gold V] Title: 로봇 청소기, Time: 56 ms, Memory: 35004 KB -BaekjoonHub

--- 백준/Gold/14503. 로봇 청소기/로봇 청소기.py
+++ 백준/Gold/14503. 로봇 청소기/로봇 청소기.py
@@ -1,43 +1,45 @@
 from collections import deque
 
-def rotate_to_clean(d):
+def turn_left(d):
     return (d + 3) % 4
 
-def go_to_back(d):
+def turn_back(d):
     return (d + 2) % 4
 
-def clean_room(r, c, d, n, m, room):
+def clean_room(r, c, d, my_map, n, m):
     room_cnt = 1
-    room[r][c] = 2
-    queue = deque([[r, c, d]])
+    my_map[r][c] = 2
+    queue = deque()
+    queue.append([r, c, d])
 
     while queue:
         r, c, d = queue.popleft()
         temp_d = d
 
         for i in range(4):
-            temp_d = rotate_to_clean(temp_d)
-            new_r, new_c = r + dxy[temp_d][0], c + dxy[temp_d][1]
+            temp_d = turn_left(temp_d)
+            next_r, next_c = r + dr[temp_d], c + dc[temp_d]
 
-            if  0 <= new_r < n and 0 <= new_c < m and room[new_r][new_c] == 0:
+            if 0 <= next_r < n and 0 <= next_c < m and my_map[next_r][next_c] == 0:
                 room_cnt += 1
-                room[new_r][new_c] = 2
-                queue.append([new_r, new_c, temp_d])
+                my_map[next_r][next_c] = 2
+                queue.append([next_r, next_c, temp_d])
                 break
-
-
             elif i == 3:
-                temp_d = go_to_back(d)
-                new_r, new_c = r + dxy[temp_d][0], c + dxy[temp_d][1]
-                if room[new_r][new_c] == 1:
+                temp_d = turn_back(d)
+                next_r, next_c = r + dr[temp_d], c + dc[temp_d]
+                if my_map[next_r][next_c] == 1:
                     return room_cnt
-                queue.append([new_r, new_c, d])
-
+                queue.append([next_r, next_c, d])
     return room_cnt
+
 
 
 N, M = map(int, input().split())
 r, c, d = map(int, input().split())
-room = [list(map(int, input().split())) for _ in range(N)]
-dxy = [[-1, 0], [0, 1], [1, 0], [0, -1]] # 북: 0, 동: 1, 남: 2, 서: 3
-print(clean_room(r, c, d, N, M, room))
+my_map = [list(map(int, input().split())) for _ in range(N)]
+
+dr = [-1, 0, 1, 0]
+dc = [0, 1, 0, -1]
+
+print(clean_room(r, c, d, my_map, N, M))
