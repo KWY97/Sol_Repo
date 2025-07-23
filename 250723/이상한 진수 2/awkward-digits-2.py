883f0fa54ce5e403b1ce4061376e3f2688c8011a
KWY97 <kwy1547@gmail.com>
2025-07-23 15:09:36 +0900

    Title: 이상한 진수 2, Time: 45ms, Memory: 16MB, Status: Passed - Codetree

--- 250723/이상한 진수 2/awkward-digits-2.py
+++ 250723/이상한 진수 2/awkward-digits-2.py
@@ -1,20 +1,16 @@
-a = int(input())
-n = len(str(a))
-max_num_bi = 0
+def to_decimal(li_num, length):
+    decimal = 0
+    for i in range(length):
+        decimal = 2 * decimal + li_num[i]
+    return decimal
 
-for i in range(n):
-    if (str(a))[i] == '1':
-        temp = a - 10**(n-1-i)
-    elif (str(a))[i] == '0':
-        temp = a + 10**(n-1-i)
-
-    if max_num_bi < temp:
-        max_num_bi = temp
-
-l = len(str(max_num_bi))
+a = list(map(int, input()))
+n = len(a)
 ans = 0
 
-for i in range(l-1, -1, -1):
-    ans += int((str(max_num_bi))[i]) * 2**(l-1-i)
+for i in range(n):
+    a[i] = 1 - a[i]
+    ans = max(ans, to_decimal(a, n))
+    a[i] = 1 - a[i]
 
 print(ans)
