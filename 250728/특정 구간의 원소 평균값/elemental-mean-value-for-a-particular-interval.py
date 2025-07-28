f633a974f25c5ace016a730e789cb3b41af207cb
KWY97 <kwy1547@gmail.com>
2025-07-28 15:18:42 +0900

    Title: 특정 구간의 원소 평균값, Time: 59ms, Memory: 17MB, Status: Wrong Answer - Codetree

---
+++ 250728/특정 구간의 원소 평균값/elemental-mean-value-for-a-particular-interval.py
@@ -0,0 +1,15 @@
+N = int(input())
+numbers = list(map(int, input().split()))
+cnt = 0
+
+for i in range(N):
+    for j in range(i, N):
+        temp = 0
+        for k in range(i, j+1):
+            temp += numbers[k]
+
+        average = temp / (j - i + 1)
+        if average in numbers:
+            cnt += 1
+
+print(cnt)
