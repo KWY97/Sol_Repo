83811e401243b43b5a1edc2380a289dc9212c03d
KWY97 <kwy1547@gmail.com>
2025-07-28 13:42:57 +0900

    Title: 구간 중 최대 합, Time: 41ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 250728/구간 중 최대 합/max-sum-of-subarray.py
@@ -0,0 +1,13 @@
+n, k = map(int, input().split())
+numbers = list(map(int, input().split()))
+ans = None
+
+for i in range(n-k+1):
+    temp_sum = numbers[i]
+    for j in range(i+1, i+k):
+        temp_sum += numbers[j]
+
+        if ans is None or ans < temp_sum:
+            ans = temp_sum
+
+print(ans)
