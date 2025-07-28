264717f88af6b80afaf3ba2b70e5f617a4326b0e
KWY97 <kwy1547@gmail.com>
2025-07-28 14:29:59 +0900

    Title: G or H 3, Time: 78ms, Memory: 18MB, Status: Runtime Error - Codetree

--- 250728/G or H 3/G-or-H-3.py
+++ 250728/G or H 3/G-or-H-3.py
@@ -3,26 +3,22 @@
 # 사진 찍어 얻을 수 있는 최대 점수
 
 N, K = map(int, input().split())
-infos = [0] * 40
+infos = [0] * 30
 max_loc = 0
 ans = None
 
 for _ in range(N):
     loc, alphabet = input().split()
-    infos[int(loc)] = alphabet
+    if alphabet == 'G':
+        infos[int(loc)] = 1
+    elif alphabet == 'H':
+        infos[int(loc)] = 2
     max_loc = max(max_loc, int(loc))
 
-print(infos)
-
-for i in range(max_loc+1):
-    if infos[i] != 0:
-        temp = 0
-        for j in range(i, i+K+1):
-            if infos[j] == 'G':
-                temp += 1
-            elif infos[j] == 'H':
-                temp += 2
-
+for i in range(1, max_loc-K+2):
+    temp = 0
+    for j in range(i, i+K+1):
+        temp += infos[j]
         if ans is None or ans < temp:
             ans = temp
 
