0b167b971a94dec10301273a52fd5f4f95f9edbb
KWY97 <kwy1547@gmail.com>
2025-08-08 18:07:21 +0900

    [Gold V] Title: 토마토, Time: 2568 ms, Memory: 116868 KB -BaekjoonHub

---
+++ 백준/Gold/7576. 토마토/토마토.py
@@ -0,0 +1,38 @@
+from collections import deque
+
+M, N = map(int, input().split())
+tomato = [list(map(int, input().split())) for _ in range(N)]
+q = deque()
+max_day = 0
+
+dx = [0, 1, 0, -1]
+dy = [1, 0, -1, 0]
+
+for i in range(N):
+    for j in range(M):
+        if tomato[i][j] == 1:
+            q.append([i, j, 0])
+
+while q:
+    x, y, d = q.popleft()
+    max_day = max(max_day, d)
+
+    for i in range(4):
+        nx = x + dx[i]
+        ny = y + dy[i]
+
+        if nx < 0 or nx >= N or ny < 0 or ny >= M:
+            continue
+
+        if tomato[nx][ny] != 0:
+            continue
+
+        tomato[nx][ny] = 1
+        q.append([nx, ny, d+1])
+
+for row in tomato:
+    if 0 in row:
+        print(-1)
+        break
+else:
+    print(max_day)
