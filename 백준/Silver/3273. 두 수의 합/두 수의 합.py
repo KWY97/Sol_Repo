ddb36df23e33f9fc90fb5a67cc8a8ad7e6430b6f
KWY97 <kwy1547@gmail.com>
2025-08-09 22:05:14 +0900

    [Silver III] Title: 두 수의 합, Time: 64 ms, Memory: 42660 KB -BaekjoonHub

--- 백준/Silver/3273. 두 수의 합/두 수의 합.py
+++ 백준/Silver/3273. 두 수의 합/두 수의 합.py
@@ -1,19 +1,25 @@
+def solution(n, numbers, x):
+    ans = 0
+    numbers.sort()
+
+    left = 0
+    right = n-1
+
+    while left < right:
+        temp = numbers[left] + numbers[right]
+
+        if temp == x:
+            ans += 1
+            left += 1
+        elif temp < x:
+            left += 1
+        else:
+            right -= 1
+
+    return ans
+
 n = int(input())
 numbers = list(map(int, input().split()))
 x = int(input())
-count = 0
-numbers.sort()
 
-left = 0
-right = n-1
-
-while left < right:
-    temp = numbers[left] + numbers[right]
-    if temp == x:
-        count += 1
-        left += 1
-    elif temp > x:
-        right -= 1
-    else: left += 1
-
-print(count)
+print(solution(n, numbers, x))
