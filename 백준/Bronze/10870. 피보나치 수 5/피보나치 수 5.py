9a7b63d40975149035c5ab969436c0999a949968
KWY97 <kwy1547@gmail.com>
2025-08-11 01:42:36 +0900

    [Bronze II] Title: 피보나치 수 5, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.py
@@ -0,0 +1,11 @@
+def fibo(n):
+    memo = {0: 0, 1: 1, 2: 1}
+
+    for i in range(3, n + 1):
+        memo[i] = memo[i - 1] + memo[i - 2]
+
+    return memo[n]
+
+
+n = int(input())
+print(fibo(n))
