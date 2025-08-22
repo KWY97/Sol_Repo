ef8d1b4944f09f0566f89690aaa465fd17232f12
KWY97 <kwy1547@gmail.com>
2025-08-22 10:55:02 +0900

    [Silver IV] Title: 큐, Time: 64 ms, Memory: 34924 KB -BaekjoonHub

--- 백준/Silver/10845. 큐/큐.py
+++ 백준/Silver/10845. 큐/큐.py
@@ -7,29 +7,17 @@ n = int(input())
 q = deque()
 
 for _ in range(n):
-    order = list(input().split())
+    order = input().split()
 
     if order[0] == 'push':
         q.append(order[1])
     elif order[0] == 'pop':
-        if q:
-            print(q.popleft())
-        else:
-            print(-1)
+        print(q.popleft() if q else -1)
     elif order[0] == 'size':
         print(len(q))
     elif order[0] == 'front':
-        if q:
-            print(q[0])
-        else:
-            print(-1)
+        print(q[0] if q else -1)
     elif order[0] == 'back':
-        if q:
-            print(q[-1])
-        else:
-            print(-1)
+        print(q[-1] if q else -1)
     elif order[0] == 'empty':
-        if q:
-            print(0)
-        else:
-            print(1)
+        print(0 if q else 1)
