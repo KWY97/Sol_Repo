9cd8d61f053ebadf8bcb9fe08363c7ab2f872515
KWY97 <kwy1547@gmail.com>
2025-08-25 22:43:06 +0900

    [Bronze II] Title: Hashing, Time: 40 ms, Memory: 32412 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.py
+++ 백준/Bronze/15829. Hashing/Hashing.py
@@ -1,7 +1,9 @@
 L = int(input())
 s = input()
+M = 1234567891
+r = 31
 ans = 0
 
 for i in range(L):
-    ans += (ord(s[i]) - 96) * (31 ** i)
-print(ans)
+    ans += (ord(s[i]) - 96) * (r ** i)
+print(ans % M)
