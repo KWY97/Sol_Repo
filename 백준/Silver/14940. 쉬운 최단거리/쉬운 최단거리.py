cd87d7baee39268b1da0960ecae742c1ef126129
KWY97 <kwy1547@gmail.com>
2025-08-26 14:32:54 +0900

    [Silver I] Title: 쉬운 최단거리, Time: 420 ms, Memory: 41536 KB -BaekjoonHub

---
+++ 백준/Silver/14940. 쉬운 최단거리/쉬운 최단거리.py
@@ -0,0 +1,45 @@
+from collections import deque
+
+def bfs(ix, iy):
+    q.append([ix, iy])
+    ans[ix][iy] = 0
+
+    while q:
+        x, y = q.popleft()
+
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+
+            if nx < 0 or ny < 0 or nx >= n or ny >= m:
+                continue
+            if maze[nx][ny] != 1:
+                continue
+            if ans[nx][ny] != 0:
+                continue
+
+            q.append([nx, ny])
+            ans[nx][ny] = ans[x][y] + 1
+    return
+
+n, m = map(int, input().split())
+maze = [list(map(int, input().split())) for _ in range(n)]
+ans = [[0] * m for _ in range(n)]
+q = deque()
+
+dx = [0, 1, 0, -1]
+dy = [1, 0, -1, 0]
+
+for i in range(n):
+    for j in range(m):
+        if maze[i][j] == 2:
+            bfs(i, j)
+            break
+
+for i in range(n):
+    for j in range(m):
+        if maze[i][j] == 1 and ans[i][j] == 0:
+            ans[i][j] = -1
+
+for row in ans:
+    print(' '.join(map(str, row)))
