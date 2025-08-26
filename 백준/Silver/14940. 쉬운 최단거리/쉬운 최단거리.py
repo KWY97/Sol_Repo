eb0ee13df37035cc19f08d8341eb6f19e426f8e8
KWY97 <kwy1547@gmail.com>
2025-08-26 14:34:29 +0900

    [Silver I] Title: 쉬운 최단거리, Time: 400 ms, Memory: 41540 KB -BaekjoonHub

--- 백준/Silver/14940. 쉬운 최단거리/쉬운 최단거리.py
+++ 백준/Silver/14940. 쉬운 최단거리/쉬운 최단거리.py
@@ -1,4 +1,7 @@
+import sys
 from collections import deque
+
+input = sys.stdin.readline
 
 def bfs(ix, iy):
     q.append([ix, iy])
@@ -30,11 +33,15 @@ q = deque()
 dx = [0, 1, 0, -1]
 dy = [1, 0, -1, 0]
 
+found = False
 for i in range(n):
     for j in range(m):
         if maze[i][j] == 2:
             bfs(i, j)
+            found = True
             break
+    if found:
+        break
 
 for i in range(n):
     for j in range(m):
