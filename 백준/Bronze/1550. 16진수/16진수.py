f17190f5b00f41afe8bdac09d1afee6cb4215a69
KWY97 <kwy1547@gmail.com>
2025-09-05 14:11:46 +0900

    [Bronze II] Title: 16진수, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/1550. 16진수/16진수.py
+++ 백준/Bronze/1550. 16진수/16진수.py
@@ -1,2 +1,15 @@
-x = input()
-print(int(x , 16))
+s = input()
+n = len(s)
+l = [0] * n
+ans = 0
+
+for i in range(n):
+    if s[i].isdecimal():
+        l[i] = int(s[i])
+    else:
+        l[i] = ord(s[i]) - 55
+
+for j in range(n):
+    ans += l[j] * (16 ** (n-j-1))
+
+print(ans)
