321baa1103d50bf4e8ffa361c8074ba1d5184615
KWY97 <kwy1547@gmail.com>
2025-09-07 16:51:19 +0900

    [Gold IV] Title: 오큰수, Time: 816 ms, Memory: 183880 KB -BaekjoonHub

--- 백준/Gold/17298. 오큰수/오큰수.py
+++ 백준/Gold/17298. 오큰수/오큰수.py
@@ -4,19 +4,10 @@ input = sys.stdin.readline
 N = int(input())
 nums = list(map(int, input().strip().split()))
 stack = []
-ans = [0] * N
+ans = [-1] * N
 
 for i, v in enumerate(nums):
-    if not stack:
-        stack.append([i, v])
-    else:
-        while stack and stack[-1][1] < v:
-            pop_i, _ = stack.pop()
-            ans[pop_i] = v
-        stack.append([i, v])
-
-for j in range(N):
-    if ans[j] == 0:
-        ans[j] = -1
-
+    while stack and nums[stack[-1]] < v:
+        ans[stack.pop()] = v
+    stack.append(i)
 print(' '.join(map(str, ans)))
