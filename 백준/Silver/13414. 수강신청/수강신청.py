7e2e1a2faeb360b26a575dbf7530192ea44fadd8
KWY97 <kwy1547@gmail.com>
2025-09-11 16:26:17 +0900

    [Silver III] Title: 수강신청, Time: 424 ms, Memory: 76900 KB -BaekjoonHub

--- 백준/Silver/13414. 수강신청/수강신청.py
+++ 백준/Silver/13414. 수강신청/수강신청.py
@@ -2,11 +2,11 @@ import sys
 input = sys.stdin.readline
 
 K, L = map(int, input().split())
-students_li = [input().strip() for _ in range(L)]
 students_dict = {}
 cnt = 0
 
-for i, v in enumerate(students_li):
+for i in range(L):
+    v = input().strip()
     students_dict[v] = i
 
 sorted_students_dict = sorted(students_dict.items(), key = lambda x: x[1])
