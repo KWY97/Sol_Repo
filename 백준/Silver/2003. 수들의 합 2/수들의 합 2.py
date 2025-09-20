988a869ca62d767a5cd286bdaed09240ba8b1737
KWY97 <kwy1547@gmail.com>
2025-09-20 23:30:43 +0900

    [Silver IV] Title: 수들의 합 2, Time: 40 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Silver/2003. 수들의 합 2/수들의 합 2.py
+++ 백준/Silver/2003. 수들의 합 2/수들의 합 2.py
@@ -4,16 +4,18 @@ nums = list(map(int, input().split()))
 cnt = 0
 start = 0
 end = 0
+temp = nums[0]
 
 while end < N:
-    temp = sum(nums[start:end+1])
-
     if temp == M:
         cnt += 1
-        end += 1
-    elif temp > M:
+
+    if temp >= M:
+        temp -= nums[start]
         start += 1
     else:
         end += 1
+        if end < N:
+            temp += nums[end]
 
 print(cnt)
