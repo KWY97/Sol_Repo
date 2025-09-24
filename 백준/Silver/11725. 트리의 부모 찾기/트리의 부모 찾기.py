02dec4cf420b02546ec66fa56f2d72480f68106b
KWY97 <kwy1547@gmail.com>
2025-09-24 20:39:29 +0900

    [Silver II] Title: 트리의 부모 찾기, Time: 2792 ms, Memory: 57048 KB -BaekjoonHub

--- 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.py
+++ 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.py
@@ -1,22 +1,25 @@
-import sys
-sys.setrecursionlimit(10**5)
+from collections import deque
 
-def my_dfs(node):
-    for i in adjL[node]:
-        if parent[i] == 0:
-            parent[i] = node
-            my_dfs(i)
+def bfs(root_node):
+    q = deque([root_node])
+    parent[root_node] = -1
+
+    while q:
+        cur = q.popleft()
+        for next_node in adjL[cur]:
+            if parent[next_node] == 0:
+                parent[next_node] = cur
+                q.append(next_node)
 
 N = int(input())
 adjL = [[] for _ in range(N+1)]
 parent = [0] * (N+1)
 root = 1
-for _ in range(N-1):
-    node1, node2 = map(int, input().split())
-    adjL[node1].append(node2)
-    adjL[node2].append(node1)
 
-my_dfs(root)
-ans = parent[2:]
-for parent_node in ans:
-    print(parent_node)
+for _ in range(N-1):
+    a, b = map(int, input().split())
+    adjL[a].append(b)
+    adjL[b].append(a)
+
+bfs(root)
+print('\n'.join(map(str, parent[2:])))
