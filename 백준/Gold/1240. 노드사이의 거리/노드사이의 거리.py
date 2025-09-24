5020e2da6bd4d286ce9616c97d2840e587cf046e
KWY97 <kwy1547@gmail.com>
2025-09-25 00:01:24 +0900

    [Gold V] Title: 노드사이의 거리, Time: 196 ms, Memory: 34944 KB -BaekjoonHub

--- 백준/Gold/1240. 노드사이의 거리/노드사이의 거리.py
+++ 백준/Gold/1240. 노드사이의 거리/노드사이의 거리.py
@@ -1,29 +1,30 @@
 from collections import deque
+import sys
+input = sys.stdin.readline
 
 def bfs(start, target):
-    visited = [0] * (N+1)
-    visited[start] = 1
-    q = deque()
-    q.append([start, 0])
+    visited = [False] * (N + 1)
+    visited[start] = True
+    q = deque([(start, 0)])
 
     while q:
-        cur_node, cur_distance = q.popleft()
-        if cur_node == target:
-            return cur_distance
+        node, dist = q.popleft()
+        if node == target:
+            return dist
 
-        for next_node, next_distance in adjL[cur_node]:
-            if visited[next_node] == 0:
-                visited[next_node] = 1
-                q.append([next_node, cur_distance + next_distance])
+        for neighbor, weight in graph[node]:
+            if not visited[neighbor]:
+                visited[neighbor] = True
+                q.append((neighbor, dist + weight))
 
 N, M = map(int, input().split())
-adjL = [[] for _ in range(N+1)]
+graph = [[] for _ in range(N + 1)]
 
-for _ in range(N-1):
-    node1, node2, d = map(int, input().split())
-    adjL[node1].append([node2, d])
-    adjL[node2].append([node1, d])
+for _ in range(N - 1):
+    a, b, w = map(int, input().split())
+    graph[a].append((b, w))
+    graph[b].append((a, w))
 
 for _ in range(M):
-    start_node, target_node = map(int, input().split())
-    print(bfs(start_node, target_node))
+    u, v = map(int, input().split())
+    print(bfs(u, v))
