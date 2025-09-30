45db160486d7acff6bde2ed080a36b542d99074e
KWY97 <kwy1547@gmail.com>
2025-09-30 15:22:03 +0900

    [Silver V] Title: 행렬 곱셈, Time: 108 ms, Memory: 110908 KB -BaekjoonHub

--- 백준/Silver/2740. 행렬 곱셈/행렬 곱셈.py
+++ 백준/Silver/2740. 행렬 곱셈/행렬 곱셈.py
@@ -1,14 +1,15 @@
 N, M = map(int, input().split())
 A = [list(map(int, input().split())) for _ in range(N)]
 
-m, K = map(int, input().split())
-B = [list(map(int, input().split())) for _ in range(m)]
+M, K = map(int, input().split())
+B = [list(map(int, input().split())) for _ in range(M)]
 
 ans = [[0] * K for _ in range(N)]
 
-for i in range(N):
-    for j in range(K):
-        for k in range(M):
-            ans[i][j] += A[i][k] * B[k][j]
+for n in range(N):
+    for k in range(K):
+        for m in range(M):
+            ans[n][k] += A[n][m] * B[m][k]
+
 for line in ans:
     print(' '.join(map(str, line)))
