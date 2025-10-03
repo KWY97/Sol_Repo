356c79038b32dd1ce7da5827e397b69331559d09
KWY97 <kwy1547@gmail.com>
2025-10-03 23:22:43 +0900

    [Silver V] Title: 돌 게임 2, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/9656. 돌 게임 2/돌 게임 2.py
+++ 백준/Silver/9656. 돌 게임 2/돌 게임 2.py
@@ -1,3 +1,15 @@
 N = int(input())
-if N % 2 == 0: print("SK")
-else: print("CY")
+# 마지막 돌 가져가면 진다
+memo =  {
+    1: 'CY',
+    2: 'SK',
+    3: 'CY'
+}
+
+for i in range(4, N+1):
+    if memo[i-1] == 'SK' or memo[i-3] == 'SK':
+        memo[i] = 'CY'
+    else:
+        memo[i] = 'SK'
+
+print(memo[N])
