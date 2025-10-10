12cf3f6a947d6435ff1028371f1e8b32cfade6d8
kwy97 <kwy1547@gmail.com>
2025-10-10 10:29:06 +0900

    [Silver IV] Title: 차집합, Time: 684 ms, Memory: 137820 KB -BaekjoonHub

--- 백준/Silver/1822. 차집합/차집합.py
+++ 백준/Silver/1822. 차집합/차집합.py
@@ -1,3 +1,4 @@
+# 딕셔너리 풀이
 N, M = map(int, input().split())
 A = list(map(int, input().split()))
 B = list(map(int, input().split()))
@@ -18,3 +19,35 @@ if ans:
     print(' '.join(map(str, ans)))
 else:
     print(0)
+
+
+# 투포인터 풀이
+# N, M = map(int, input().split())
+# A = list(map(int, input().split()))
+# B = list(map(int, input().split()))
+#
+# i, j = 0, 0
+# ans = []
+#
+# A.sort()
+# B.sort()
+#
+# while i < N and j < M:
+#     if A[i] < B[j]:
+#         ans.append(A[i])
+#         i += 1
+#     elif A[i] == B[j]:
+#         i += 1
+#         j += 1
+#     else:
+#         j += 1
+#
+# while i < N:
+#     ans.append(A[i])
+#     i += 1
+#
+# if ans:
+#     print(len(ans))
+#     print(' '.join(map(str, ans)))
+# else:
+#     print(0)
