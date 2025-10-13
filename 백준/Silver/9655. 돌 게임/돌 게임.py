d0f0d2adfd5565dd342e73c94a52dad67a0522e5
KWY97 <kwy1547@gmail.com>
2025-10-13 12:26:19 +0900

    [Silver V] Title: 돌 게임, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/9655. 돌 게임/돌 게임.py
+++ 백준/Silver/9655. 돌 게임/돌 게임.py
@@ -1,12 +1,13 @@
+def dp(n):
+    if n in memo:
+        return memo[n]
+    for i in range(3, n+1):
+        memo[i] = memo[i-2]
+    return memo[n]
+
 N = int(input())
-dp = [0]*1001
-
-# SK == 0, CY == 1
-dp[1] = 0
-dp[2] = 1
-dp[3] = 0
-
-for i in range(4, N+1):
-    dp[i] = (dp[i-1] + 1) % 2  # 또는 dp[i] = (dp[i-3] + 1) % 2
-
-print('SK' if dp[N] == 0 else 'CY')
+memo = {
+    1: 'SK',
+    2: 'CY'
+}
+print(dp(N))
