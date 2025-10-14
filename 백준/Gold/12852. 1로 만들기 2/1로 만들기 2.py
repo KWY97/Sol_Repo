da38a04ccd3a3fe8e8c8a581cbeb26fe13050b91
KWY97 <kwy1547@gmail.com>
2025-10-14 19:30:18 +0900

    [Gold V] Title: 1로 만들기 2, Time: 568 ms, Memory: 79784 KB -BaekjoonHub

---
+++ 백준/Gold/12852. 1로 만들기 2/1로 만들기 2.py
@@ -0,0 +1,28 @@
+N = int(input())
+memo = [0] * (N+1)
+prev = [0] * (N+1)
+
+for i in range(2, N+1):
+    memo[i] = memo[i-1] + 1
+    prev[i] = i-1
+
+    if i % 2 == 0 and memo[i] > memo[i//2] + 1:
+        memo[i] = memo[i//2] + 1
+        prev[i] = i // 2
+
+    if i % 3 == 0 and memo[i] > memo[i//3] + 1:
+        memo[i] = memo[i//3] + 1
+        prev[i] = i // 3
+
+print(memo[N])
+path = []
+cur = N
+
+while cur != 0:
+    path.append(cur)
+    cur = prev[cur]
+
+    if cur == 0:
+        break
+
+print(' '.join(map(str, path)))
