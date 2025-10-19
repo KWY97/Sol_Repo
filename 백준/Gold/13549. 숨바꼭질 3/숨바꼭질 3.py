05e1912513cba8df6887178e861e095230a62cb7
KWY97 <kwy1547@gmail.com>
2025-10-19 18:56:07 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 116 ms, Memory: 40856 KB -BaekjoonHub

--- 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.py
+++ 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.py
@@ -3,34 +3,30 @@ input = sys.stdin.readline
 
 from collections import deque
 
-def my_bfs(start, end):
+def bfs(start, end):
+    queue = deque()
     queue.append(start)
-    visited[start] = 0
+
+    visited = [False] * 200001
+    visited[start] = True
+
+    line = [0] * 200001
 
     while queue:
         current = queue.popleft()
 
         if current == end:
             return line[current]
-        
-        for next in [current * 2, current - 1, current + 1]:
 
-            if next < 0 or next >= 200001 or visited[next] != -1:
-                continue
+        for next in (current * 2, current - 1, current + 1):
+            if 0 <= next <= 200000 and not visited[next]:
+                if next == current * 2:
+                    line[next] = line[current]
+                else:
+                    line[next] = line[current] + 1
 
-            if next == current * 2:
-                line[next] = line[current]
-            else:
-                line[next] = line[current] + 1
-
-            queue.append(next)
-            visited[next] = visited[current] + 1
-
+                queue.append(next) 
+                visited[next] = True
 
 N, K = map(int, input().split())
-visited = [-1] * 2000001
-line = [0] * 200001
-queue = deque()
-
-
-print(my_bfs(N, K))
+print(bfs(N, K))
