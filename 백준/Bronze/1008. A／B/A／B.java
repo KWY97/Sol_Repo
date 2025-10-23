b587b8a78df072e43cebcfb68a93b0447bb0e173
KWY97 <kwy1547@gmail.com>
2025-10-23 21:45:36 +0900

    [Bronze V] Title: A/B, Time: 180 ms, Memory: 17972 KB -BaekjoonHub

--- 백준/Bronze/1008. A／B/A／B.java
+++ 백준/Bronze/1008. A／B/A／B.java
@@ -3,11 +3,13 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        
-        double a = sc.nextDouble();
-        int b = sc.nextInt();
+        double A;
+        double B;
 
-        System.out.println(a / b);
+        Scanner sc = new Scanner(System.in);
+        A = sc.nextDouble();
+        B = sc.nextDouble();
+
+        System.out.println(A / B);
     }
 }
