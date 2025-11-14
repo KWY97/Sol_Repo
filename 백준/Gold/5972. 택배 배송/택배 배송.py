704ef3f5aff6e18749e933a7026379a09303d341
KWY97 <kwy1547@gmail.com>
2025-11-14 18:04:07 +0900

    [Gold V] Title: 택배 배송, Time: 284 ms, Memory: 126820 KB -BaekjoonHub

--- 백준/Gold/5972. 택배 배송/택배 배송.py
+++ 백준/Gold/5972. 택배 배송/택배 배송.py
@@ -1,28 +1,34 @@
 import heapq
 from collections import defaultdict
 
-def dijkstra(graph, start, target):
-    costs = {}
+def dijkstra(start):
+    dist = [INF] * (N+1)
     pq = []
     heapq.heappush(pq, (0, start))
 
     while pq:
-        cur_cost, cur_v = heapq.heappop(pq)
-        if cur_v not in costs:
-            costs[cur_v] = cur_cost
+        cur_cost, cur_node = heapq.heappop(pq)
 
-            for cost, next_v in graph[cur_v]:
-                next_cost = cur_cost + cost
-                heapq.heappush(pq, (next_cost, next_v))
+        if dist[cur_node] < cur_cost:
+            continue
 
-    return costs[target]
+        for next_node, cost in graph[cur_node]:
+            next_cost = cur_cost + cost
+            if dist[next_node] > next_cost:
+                dist[next_node] = next_cost
+                heapq.heappush(pq, (next_cost, next_node))
+
+    return dist[N]
+
 
 N, M = map(int, input().split())
 graph = defaultdict(list)
+INF = int(10e9)
 
 for _ in range(M):
     u, v, w = map(int, input().split())
-    graph[u].append((w, v))
-    graph[v].append((w, u))
+    graph[u].append((v, w))
+    graph[v].append((u, w))
 
-print(dijkstra(graph, 1, N))
+ans = dijkstra(1)
+print(ans)
