2c61dea048ed5c809eaf3e2e3b411d475bba62c2
KWY97 <kwy1547@gmail.com>
2025-11-14 18:38:46 +0900

    [Gold V] Title: 간선 이어가기 2, Time: 208 ms, Memory: 56096 KB -BaekjoonHub

--- 백준/Gold/14284. 간선 이어가기 2/간선 이어가기 2.py
+++ 백준/Gold/14284. 간선 이어가기 2/간선 이어가기 2.py
@@ -1,37 +1,39 @@
+import sys
+input = sys.stdin.readline
 import heapq
 from collections import defaultdict
 
 def dijkstra(start, target):
     dist = [INF] * (n+1)
-    dist[start] = 0
-    pq = [(0, start)]
+    pq = []
+    heapq.heappush(pq, (0, start))
 
     while pq:
-        cur_cost, u = heapq.heappop(pq)
+        cur_w, cur_v = heapq.heappop(pq)
 
-        if cur_cost > dist[u]:
+        if dist[cur_v] < cur_w:
             continue
 
-        if u == target:
-            return cur_cost
+        if cur_v == target:
+            return cur_w
 
-        for v, w in graph[u]:
-            next_cost = cur_cost + w
-            if next_cost < dist[v]:
-                dist[v] = next_cost
-                heapq.heappush(pq, (next_cost, v))
+        for next_v, w in graph[cur_v]:
+            next_w = cur_w + w
 
+            if dist[next_v] > next_w:
+                dist[next_v] = next_w
+                heapq.heappush(pq, (next_w, next_v))
     return dist[target]
 
 n, m = map(int, input().split())
 graph = defaultdict(list)
 INF = int(10e9)
+
 for _ in range(m):
     u, v, w = map(int, input().split())
     graph[u].append((v, w))
     graph[v].append((u, w))
 
 s, t = map(int, input().split())
-ans = dijkstra(s, t)
-print(ans)
 
+print(dijkstra(s, t))
