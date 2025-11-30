e1eb35808794349986a9573416d4ab3977d5b577
KWY97 <kwy1547@gmail.com>
2025-11-30 19:38:36 +0900

    [Bronze III] Title: 알람 시계, Time: 192 ms, Memory: 18432 KB -BaekjoonHub

---
+++ 백준/Bronze/2884. 알람 시계/알람 시계.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int hour = sc.nextInt();
+        int minute = sc.nextInt();
+
+        if (minute >= 45) {
+            System.out.println(hour + " " + (minute - 45));
+        } else {
+            if (hour != 0) {
+                System.out.println(hour-1 + " " + (minute + 15));
+            } else {
+                System.out.println(23 + " " + (minute + 15));
+            }
+        }
+    }
+}
