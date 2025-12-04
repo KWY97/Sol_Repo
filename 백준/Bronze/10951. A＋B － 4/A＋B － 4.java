835e69eaad3967234f8855f697538b4d1c846a77
KWY97 <kwy1547@gmail.com>
2025-12-04 15:50:40 +0900

    [Bronze V] Title: A+B - 4, Time: 188 ms, Memory: 17804 KB -BaekjoonHub

--- 백준/Bronze/10951. A＋B － 4/A＋B － 4.java
+++ 백준/Bronze/10951. A＋B － 4/A＋B － 4.java
@@ -5,11 +5,14 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        while (sc.hasNextInt()) {
-            int a = sc.nextInt();
-            int b = sc.nextInt();
-            System.out.println(a+b);
+        while (true) {
+            try {
+                int a = sc.nextInt();
+                int b = sc.nextInt();
+                System.out.println(a + b);
+            } catch (Exception e) {
+                break;
+            }
         }
-        sc.close();
     }
 }
