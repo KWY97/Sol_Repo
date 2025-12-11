befae2271f1c35170d0d7cc72def79c5f5149728
KWY97 <kwy1547@gmail.com>
2025-12-11 17:29:51 +0900

    [Bronze III] Title: 공 넣기, Time: 200 ms, Memory: 18064 KB -BaekjoonHub

--- 백준/Bronze/10810. 공 넣기/공 넣기.java
+++ 백준/Bronze/10810. 공 넣기/공 넣기.java
@@ -9,25 +9,22 @@ public class Main {
         int m = sc.nextInt();
 
         int[] basket = new int[n];
-        for (int i = 0; i < n; i++) {
-            basket[i] = 0;
-        }
 
         for (int i = 0; i < m; i++) {
-            int a = sc.nextInt();
-            int b = sc.nextInt();
-            int c = sc.nextInt();
+            int start = sc.nextInt();
+            int end = sc.nextInt();
+            int value = sc.nextInt();
 
-            for (int j = a-1; j < b; j++) {
-                basket[j] = c;
+            for (int j = start-1; j < end; j++) {
+                basket[j] = value;
             }
         }
 
         for (int i = 0; i < n; i++) {
-            System.out.print(basket[i]);
-            if (i != n-1) {
+            if (i > 0) {
                 System.out.print(" ");
             }
+            System.out.print(basket[i]);
         }
     }
 }
