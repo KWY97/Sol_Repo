b3b61103f01766377a57d3ba69e33b5723951f51
KWY97 <kwy1547@gmail.com>
2025-12-13 20:15:23 +0900

    [Bronze V] Title: Multiply, Time: 788 ms, Memory: 58376 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/22193. Multiply/Multiply.java
+++ 백준/Bronze/22193. Multiply/Multiply.java
@@ -1,15 +1,19 @@
 import java.util.Scanner;
+import java.math.BigInteger;
 
 public class Main {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int m = sc.nextInt();
-        
-        int a = sc.nextInt();
-        int b = sc.nextInt();
 
-        System.out.println(a * b);
+        int n = sc.nextInt(); // 자릿수 정보 (실제로 계산엔 안 씀)
+        int m = sc.nextInt();
+
+        BigInteger a = new BigInteger(sc.next());
+        BigInteger b = new BigInteger(sc.next());
+
+        System.out.println(a.multiply(b));
+
+        sc.close();
     }
 }
