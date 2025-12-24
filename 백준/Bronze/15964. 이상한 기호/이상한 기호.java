4dc2fb4b7700b4b2061a920114050d5d856bae4d
KWY97 <kwy1547@gmail.com>
2025-12-25 00:05:17 +0900

    [Bronze V] Title: 이상한 기호, Time: 164 ms, Memory: 17704 KB, Score: 30 point -BaekjoonHub

---
+++ 백준/Bronze/15964. 이상한 기호/이상한 기호.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    static int calculate(int a, int b) {
+        int ans = (a + b) * (a - b);
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        System.out.println(calculate(a, b));
+    }
+}
