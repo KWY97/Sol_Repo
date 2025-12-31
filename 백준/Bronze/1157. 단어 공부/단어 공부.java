93b19276916a5c13bf05b64ec2e5303aabe302b4
KWY97 <kwy1547@gmail.com>
2025-12-31 18:28:57 +0900

    [Bronze I] Title: 단어 공부, Time: 508 ms, Memory: 34140 KB -BaekjoonHub

--- 백준/Bronze/1157. 단어 공부/단어 공부.java
+++ 백준/Bronze/1157. 단어 공부/단어 공부.java
@@ -1,32 +1,37 @@
-import java.util.HashMap;
-import java.util.Map;
 import java.util.Scanner;
 
 public class Main {
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        char[] arr = sc.nextLine().toUpperCase().toCharArray();
+        char[] s = sc.nextLine().toUpperCase().toCharArray();
+        int[] english = new int[26];
 
-        Map<Character, Integer> eng = new HashMap<>();
-        for (char c = 'A'; c <= 'Z'; c++) eng.put(c, 0);
-
-        for (char c : arr) eng.put(c, eng.get(c) + 1);
+        for (int i = 0; i < s.length; i++) {
+            int idx = s[i] - 'A';
+            english[idx] += 1;
+        }
 
         int max = 0;
-        for (int v : eng.values()) {
-            if (v > max) max = v;
+        for (int i = 0; i < 26; i++) {
+            max = Math.max(max, english[i]);
         }
 
         int count = 0;
-        char result = '?';
-        for (Map.Entry<Character, Integer> e : eng.entrySet()) {
-            if (e.getValue() == max) {
-                result = e.getKey();
+        int idx = -1;
+
+        for (int i = 0; i < 26; i++) {
+            if (english[i] == max) {
                 count++;
+                idx = i;
             }
         }
 
-        System.out.println(count == 1 ? result : '?');
+        if (count != 1) {
+            System.out.println('?');
+        } else {
+            System.out.println((char) (idx + 'A'));
+        }
     }
 }
